9de991704a643c966d0e6956dbf874b7b48317e2
Michael Lagajino <mlagajin@gmail.com>
2017-08-14 12:54:26 -0400

    Change var

---
+++ app.js
@@ -0,0 +1,12 @@
+// Creating a script with 2 variables, each assigned to a number.
+var x = 5;
+var y = 10;
+
+console.log(x + 10);
+
+// Creating a script with 2 variables, each assigned to a string.
+
+var a = "Pickle"
+var b = "Rick"
+
+console.log(a + " " + b);
