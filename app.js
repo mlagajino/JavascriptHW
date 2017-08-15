0dfe608c265c008d3f122f4979a4d9e3a58ba8fb
Michael Lagajino <mlagajin@gmail.com>
2017-08-15 13:17:36 -0400

    Question 5

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// Question 1: Creating a script with 2 variables, each assigned to a number.
+// Question 1: Create a script with 2 variables, each assigned to a number.
 var x = 5;
 var y = 10;
 
 console.log(x + 10);
 
-// Creating a script with 2 variables, each assigned to a string.
+// Create a script with 2 variables, each assigned to a string.
 
-var a = "Pickle"
+var a = "Pickle "
 var b = "Rick"
 
-console.log(a + " " + b);
+console.log(a + b);
 
 //Question 2: Creating a multidimentional array with names.
 var simpsonNames = ["Bart", "Lisa", "Homer"];
@@ -42,4 +42,21 @@ function declareName(someName) {
 //Invoke the function.
 declareName("Greg");
 
-//Question 5:
+//Question 5:Declare function openDoor then create 3 door options using if else statements.
+
+function openDoor(doorNumber) {
+	if (doorNumber === 1) {
+		return alert("car");
+	} else if (doorNumber === 2) {
+		return alert("canopener");
+	} else if (doorNumber ===  3) {
+		return alert("used bandaids");
+	} else {
+		return alert("not a valid door");
+	}
+}
+
+//Invoke the function using different door options.
+openDoor(2);
+openDoor(3);
+openDoor("szechuan sauce");
